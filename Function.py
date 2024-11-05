a75bdc20b88d72e5133e3782729f46a96566cda4
Darian <133162157+Srdariansitop@users.noreply.github.com>
2024-11-04 23:11:36 -0500

    agg sin x and cos x

--- Function.py
+++ Function.py
@@ -1,10 +1,15 @@
+import math
+
 class Function:
-    def __init__(self, function_string):
-        self.function_string = function_string
+  def __init__(self, function_string):
+    self.function_string = function_string
 
-    def evaluate(self, x, y):
-        return eval(self.function_string) 
+  def evaluate(self, x, y):
+    function_string = self.function_string.replace("sin", "math.sin")
+    function_string = function_string.replace("cos", "math.cos")
+    return eval(function_string)
+  
 
-func = Function("x**2 + 3*y")  # Define la función como 'x**2 + 3*y'
-result = func.evaluate(2, 5)  # Evalúa la función en x = 2, y = 5
+func = Function("sin(x) + y")  # Define la función como 'x**2 + 3*y'
+result = func.evaluate(0, 6)  # Evalúa la función en x = 2, y = 5
 print(f"f(2, 5) = {result}") # Imprime el resultado (f(2, 5) = 19)
